d863111148cb7f3e1ead9bced6b6fd425e47cfb6
Veronica Ni <vni1294@gmail.com>
2024-01-29 15:35:39 -0500

    Implementing searchShowsByTerm

--- src/model.ts
+++ src/model.ts
@@ -1,5 +1,7 @@
 const MISSING_IMAGE_URL = "https://tinyurl.com/missing-tv";
 const TVMAZE_API_URL = "https://api.tvmaze.com/";
+import { Ishow } from "./interfaces";
+
 
 /** Given a search term, search for tv shows that match that query.
  *
@@ -8,26 +10,19 @@ const TVMAZE_API_URL = "https://api.tvmaze.com/";
  *    (if no image URL given by API, put in a default image URL)
  */
 
-async function searchShowsByTerm(term) {
+async function searchShowsByTerm(term: string) : Promise<Ishow[]> {
   // ADD: Remove placeholder & make request to TVMaze search shows API.
-  return [
-    {
-      id: 1767,
-      name: "The Bletchley Circle",
-      summary:
-        `<p><b>The Bletchley Circle</b> follows the journey of four ordinary
-           women with extraordinary skills that helped to end World War II.</p>
-         <p>Set in 1952, Susan, Millie, Lucy and Jean have returned to their
-           normal lives, modestly setting aside the part they played in
-           producing crucial intelligence, which helped the Allies to victory
-           and shortened the war. When Susan discovers a hidden code behind an
-           unsolved murder she is met by skepticism from the police. She
-           quickly realises she can only begin to crack the murders and bring
-           the culprit to justice with her former friends.</p>`,
-      image:
-        "http://static.tvmaze.com/uploads/images/medium_portrait/147/369403.jpg"
-    }
-  ]
+    const resp = await fetch(`${TVMAZE_API_URL}search/shows?q=${term}`);
+    const showsAndScores = await resp.json();
+    console.log("searchShowsByTerm shows=", showsAndScores);
+    const showsData = showsAndScores.map(s => s.show);
+    const shows = showsData.map(s => ({
+      id: s.id,
+      name: s.name,
+      summary: s.summary,
+      image: s.image || MISSING_IMAGE_URL
+    }));
+    return shows;
 }
 
 
@@ -36,6 +31,7 @@ async function searchShowsByTerm(term) {
  */
 
 async function getEpisodesOfShow(id) {
+  //TODO: implement
 }
 
 
